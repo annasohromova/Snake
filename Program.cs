35de2513bc2318d31eb4ae0957ae8202eb58735b
annasohromova <120181353+annasohromova@users.noreply.github.com>
2023-05-08 10:47:19 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,18 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
 
-namespace Snake
+namespace uss4
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world");
+            Point p1 = new Point(1, 3, '*');
+            p1.Draw();
+
+            Point p2 = new Point(4, 5, '#');
+            p2.Draw();
+
             Console.ReadLine();
         }
+
+      
+        
     }
 }
+
