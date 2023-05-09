6ae95af34b4765586dccebed06303493350e8c2e
annasohromova <120181353+annasohromova@users.noreply.github.com>
2023-05-09 11:41:28 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,18 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
+using System.Net;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
 
-namespace Snake
+namespace uss
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world");
+            Point p1 = new Point(1, 3, '*');
+            p1.Draw();
+
+            Point p2 = new Point(4, 5, '#');
+            p2.Draw();
+
+            HorisontalLine line = new HorisontalLine();
+            line.Drow();
+
             Console.ReadLine();
         }
     }
 }
+
