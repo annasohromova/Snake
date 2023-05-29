71fb3e1b9329ac88bd538384e56b63b74ff9d027
annasohromova <120181353+annasohromova@users.noreply.github.com>
2023-05-29 15:43:00 +0300

    Add files via upload

---
+++ Heli.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NAudio.Wave;
+
+namespace Snake
+{
+    public class Heli
+    {
+        public async Task Tagaplaanis_Mangida(string Path)
+        {
+            await Task.Run(() =>
+            {
+                using (AudioFileReader audioFileReader = new AudioFileReader(Path))
+                using (IWavePlayer waveOutDevice = new WaveOutEvent { DesiredLatency = 200, Volume = 50 })
+                {
+                    waveOutDevice.Init(audioFileReader);
+                    waveOutDevice.Play();
+                    while (waveOutDevice.PlaybackState == PlaybackState.Playing)
+                    {
+                        Thread.Sleep(1000);
+                    }
+                }
+            });
+        }
+        public async Task Natuke_mangida(string Path)
+            {
+            await Task.Run(() =>
+            {
+                using (AudioFileReader audioFileReader = new AudioFileReader(Path));
+
+            });
+            }
+        
+    }
+}
+
---
+++ Player.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Snake
+{
+    public class Player
+    {
+        public string Name { get; set; }
+        public int Score { get; set; }
+
+        public Player()
+        {
+            Name = string.Empty;
+            Score = 0;
+        }
+
+        public void SetName()
+        {
+            Console.WriteLine("Введите ваше имя:");
+            Name = Console.ReadLine();
+        }
+
+        public void IncreaseScore(int points)
+        {
+            Score += points;
+        }
+
+        public void UpdateResult()
+        {
+            string result = $"{Name}: {Score}";
+            File.AppendAllText("results.txt", result + Environment.NewLine);
+        }
+    }
+}
+
