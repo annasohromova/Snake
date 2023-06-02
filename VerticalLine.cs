1d1bccb40ace10a59ba9a9fd5b836f0895ff268a
annasohromova <120181353+annasohromova@users.noreply.github.com>
2023-06-02 10:52:58 +0300

    Add files via upload

---
+++ VerticalLine.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using uss;
+
+namespace Snake
+{
+    class VerticalLine : Figure
+    {
+        public VerticalLine(int yUp, int yDown, int x, char sym)
+        {
+            pList = new List<Point>();
+            for (int y = yUp; y <= yDown; y++)
+            {
+                Point p = new Point(x, y, sym);
+                pList.Add(p);
+            }
+        }
+        public virtual void Draw()
+        {
+            Console.ForegroundColor = ConsoleColor.Red; // Устанавливаем цвет
+            foreach (Point p in pList)
+            {
+                Console.SetCursorPosition(p.x, p.y);
+                Console.Write(p.sym);
+            }
+            Console.ResetColor(); // Сбрасываем цвет
+        }
+    }
+}
